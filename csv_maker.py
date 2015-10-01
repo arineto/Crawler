9dcc977ecf9abf7a754c8d790829874b0c6e4e31
Arimatea Neto <arineto30@gmail.com>
2015-10-01 19:41:16 -0300

    Modificacoes para as novas consultas e calculando prioridade dos arquivos

--- csv_maker.py
+++ csv_maker.py
@@ -2,6 +2,7 @@
 import mmap
 import os
 import csv
+import operator
 from collections import OrderedDict
 from codecs import open
 
@@ -19,6 +20,10 @@ file_list = get_log_files_list(my_dir)
 
 movies = []
 
+q1_priority = {}
+q2_priority = {}
+q3_priority = {}
+
 q3_counter = 0
 q2_counter = 0
 q1_counter = 0
@@ -33,20 +38,50 @@ for f in file_list:
 		read_file = opened_file.read()
 		read_file = read_file.lower()
 
-		if 'bolo' in read_file and not ('leite' in read_file):
-			c1 = 1
+		# Q1 = 'movies of Kevin McKidd'
+		# Q2 = 'Martin Freeman and Nick Frost'
+		# Q3 = 'World War'
+
+		if ('movie' in read_file) or ('movies' in read_file) or ('of' in read_file) or ('kevin' in read_file) or ('mckidd' in read_file) or ('kevin mckidd') in read_file:
+			priority = 0
+			priority += read_file.count('movie')
+			priority += read_file.count('movies')
+			priority += read_file.count('of')
+			priority += read_file.count('kevin')
+			priority += read_file.count('mckidd')
+			priority += read_file.count('kevin mckidd')*5
+			q1_priority[f] = priority
 			q1_counter += 1
-		if 'bolo' in read_file and 'chocolate' in read_file:
+			c1 = 1
+		
+		if ('martin' in read_file) or ('freeman' in read_file) or ('and' in read_file) or ('nick' in read_file) or ('frost' in read_file) \
+		or ('martin freeman' in read_file) or ('nick frost' in read_file):
+			priority = 0
+			priority += read_file.count('martin')
+			priority += read_file.count('freeman')
+			priority += read_file.count('and')
+			priority += read_file.count('nick')
+			priority += read_file.count('frost')
+			priority += read_file.count('martin freeman')*5
+			priority += read_file.count('nick frost')*5
+			q2_priority[f] = priority
 			q2_counter += 1
 			c2 = 1
-		if ">8 portions" in read_file:
+		
+		if ('world' in read_file) or ('war' in read_file):
+			priority = 0
+			priority += read_file.count('world')
+			priority += read_file.count('war')
+			priority += read_file.count('world war')*5
+			q3_priority[f] = priority
 			q3_counter += 1
 			c3 = 1
+	
 	my_dict = OrderedDict()
-	my_dict['Titulo'] = f
-	my_dict['Bolo sem leite'] = c1
-	my_dict['Bolo de chocolate'] = c2
-	my_dict['Serve 8 porcoes'] = c3
+	my_dict['ID'] = f
+	my_dict['Movies of Kevin MdKidd'] = c1
+	my_dict['Martin Freeman and Nick Frost'] = c2
+	my_dict['World War'] = c3
 
 	movies.append(my_dict)
 
@@ -57,7 +92,29 @@ with open('relation_matrix.csv', 'wb') as f:  # Just use 'w' mode in 3.x
     for m in movies:
         w.writerow(m)
 
-print 'total q1 %d' % q1_counter
-print 'total q2 %d' % q2_counter
-print 'total q3 %d' % q3_counter
+
+my_range = [-1,-2,-3,-4,-5,-6,-7,-8,-9,-10]
+
+print 'Consulta 1 - Movies of Kevin Mckidd'
+print 'Total de Filmes: %d' % q1_counter
+print 'Top 10:'
+movies = sorted(q1_priority.items(), key=operator.itemgetter(1))
+for i in my_range:
+	print '%d - %s (Score: %d)' % (i+1, movies[i][0], movies[i][1])
+
+
+print 'Consulta 2 - Martin Freeman and Kevin Frost'
+print 'Total de Filmes: %d' % q2_counter
+print 'Top 10:'
+movies = sorted(q2_priority.items(), key=operator.itemgetter(1))
+for i in my_range:
+	print '%d - %s (Score: %d)' % (i+1, movies[i][0], movies[i][1])
+
+
+print 'Consulta 1 - World War'
+print 'Total de Filmes: %d' % q3_counter
+print 'Top 10:'
+movies = sorted(q3_priority.items(), key=operator.itemgetter(1))
+for i in my_range:
+	print '%d - %s (Score: %d)' % (i+1, movies[i][0], movies[i][1])
 
